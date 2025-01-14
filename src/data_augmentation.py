ee88323485c97328221ba697d7c15b2d0d9f2a4f
Ludwig Baader <ludwig.baader@googlemail.com>
2025-01-14 14:39:30 +0100

    Finish data augmentation techniques. New generation of images using GAN currently not in use, might delete later

--- src/data_augmentation.py
+++ src/data_augmentation.py
@@ -11,27 +11,38 @@ def augment_data(img_train: np.uint8, lbl_train: np.uint8, img_test: np.uint8, l
     # - blurring the images
     # - synthetic data generation -> using a GAN to generate new images
 
+    print("Augmenting the training and testing datasets...")
+    print("Generating new images through shifting")
+
     img_train_shifted, lbl_train_shifted = augment_images_shift(img_train, lbl_train, 1)
     img_test_shifted, lbl_test_shifted = augment_images_shift(img_test, lbl_test, 1)
+
+    print("Generating new images through noise addition")
 
     img_train_noisy, lbl_train_noisy = augment_images_noise(img_train, lbl_train, 0.2)
     img_test_noisy, lbl_test_noisy = augment_images_noise(img_test, lbl_test, 0.2)
 
+    print("Generating new images through blurring")
+
     img_train_blurred, lbl_train_blurred = augment_images_blur(img_train, lbl_train, 2)
     img_test_blurred, lbl_test_blurred = augment_images_blur(img_test, lbl_test, 2)
 
-    img_train_synthetic, lbl_train_synthetic = augment_images_synthetic(img_train, lbl_train, 1000)
-    img_test_synthetic, lbl_test_synthetic = augment_images_synthetic(img_test, lbl_test, 1000)
+    #print("Generating synthetic images")
 
-    img_train_augmented = np.concatenate(img_train, img_train_shifted, img_train_noisy, img_train_blurred, img_train_synthetic)
-    lbl_train_augmented = np.concatenate(lbl_train, lbl_train_shifted, lbl_train_noisy, lbl_train_blurred, lbl_train_synthetic)
-    img_test_augmented = np.concatenate(img_test, img_test_shifted, img_test_noisy, img_test_blurred, img_test_synthetic)
-    lbl_test_augmented = np.concatenate(lbl_test, lbl_test_shifted, lbl_test_noisy, lbl_test_blurred, lbl_test_synthetic)
+    #img_train_synthetic, lbl_train_synthetic = augment_images_synthetic(img_train, lbl_train, 1000)
+    #img_test_synthetic, lbl_test_synthetic = augment_images_synthetic(img_test, lbl_test, 1000)
+
+    img_train_augmented = np.concatenate((img_train, img_train_shifted, img_train_noisy, img_train_blurred))
+    lbl_train_augmented = np.concatenate((lbl_train, lbl_train_shifted, lbl_train_noisy, lbl_train_blurred))
+    img_test_augmented = np.concatenate((img_test, img_test_shifted, img_test_noisy, img_test_blurred))
+    lbl_test_augmented = np.concatenate((lbl_test, lbl_test_shifted, lbl_test_noisy, lbl_test_blurred))
 
     return ((img_train_augmented, lbl_train_augmented), (img_test_augmented, lbl_test_augmented))
 
 
 def augment_images_shift(images, labels, amount: int) -> tuple[np.uint8, np.uint8]:
+    '''Shift the given images by the specified amount of pixels in four directions (left, right, up, down). Returns four times the amount of provided images.'''
+
     shifted_images = []
 
     directions = ['left', 'right', 'up', 'down']
@@ -44,7 +55,8 @@ def augment_images_shift(images, labels, amount: int) -> tuple[np.uint8, np.uint
     return (np.array(shifted_images), labels)
 
 def shift_image(image: np.uint8, direction: str, amount: int) -> np.uint8:
-    # shift the given image by the specified amount of pixels in the specified direction
+    '''Shift the given image by the specified amount of pixels in the specified direction'''
+
     if direction == 'left':
         return shift(image, [0, -amount], cval=0)
     elif direction == 'right':
@@ -56,7 +68,8 @@ def shift_image(image: np.uint8, direction: str, amount: int) -> np.uint8:
 
 
 def augment_images_noise(images: np.uint8, labels: np.uint8, amount: float) -> tuple[np.uint8, np.uint8]:
-    # Add noise to the images
+    '''Add the speicified amount of noise to the given images. Returns the same amount of images than the provided ones.'''
+    
     for img in images:
         for x in range(img.shape[0]):
             for y in range(img.shape[1]):
@@ -67,6 +80,8 @@ def augment_images_noise(images: np.uint8, labels: np.uint8, amount: float) -> t
 
 
 def augment_images_blur(images: np.uint8, labels: np.uint8, amount: float) -> tuple[np.uint8, np.uint8]:
+    '''Blur the given images using a Gaussian filter with the specified amount of sigma. Returns the same amount of images than the provided ones.'''
+
     blurred_images = []
 
     # Apply Gaussian blur to the images
--- src/main.py
+++ src/main.py
@@ -51,7 +51,7 @@ def explore_data(img_train: np.uint8, lbl_train: np.uint8, img_test: np.uint8, l
     print(f"Training dataset class distribution: {train_counts}, Mean: {train_counts_avg}, Std: {train_counts_std}")
     print(f"Testing dataset class distribution: {test_counts}, Mean: {test_counts_avg}, Std: {test_counts_std}")
 
-    # Print 4 images in a row
+    # Print the first 4 images in the training dataset
     plt.figure(figsize=(10, 5))
     for i in range(4):
         plt.subplot(1, 4, i+1)
